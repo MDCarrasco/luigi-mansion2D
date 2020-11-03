e5b76fb445577d0e4db6c6d11d894a1f9756125d
MDCarrasco <MDCarrasco@users.noreply.github.com>
2020-11-03 22:29:02 +0100

    first commit

---
+++ characterMovement.py
@@ -0,0 +1,92 @@
+import pygame, sys
+from pygame import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.changex = 0 # value to move along x
+        self.changey = 0 # value to move along y
+
+    def move_left(self, move_x):
+        '''move player left'''
+        self.changex -= move_x
+
+    def move_right(self, move_x):
+        '''move player right'''
+        self.changex += move_x
+
+    def move_up(self, move_y):
+        '''move player up'''
+        self.changey -= move_y
+
+    def move_down(self, move_y):
+        '''move player down'''
+        self.changey += move_y
+
+    def update(self):
+        '''update player movement'''
+        self.rect.x += self.changex
+        self.rect.y += self.changey
+
+        '''boundary checking'''
+        if self.rect.x < 0:
+            self.rect.x = 0
+
+        if self.rect.y < 0:
+            self.rect.y = 0
+
+        if self.rect.x > WINDOWWIDTH - self.rect.width:
+            self.rect.x = WINDOWWIDTH - self.rect.width
+
+        if self.rect.y > WINDOWHEIGHT - self.rect.height:
+            self.rect.y = WINDOWHEIGHT - self.rect.height
+
+WINDOWWIDTH = 500
+WINDOWHEIGHT = 400
+FPS = 60
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+pygame.init()
+FPSCLOCK = pygame.time.Clock()
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption('The Lone Character')
+active_sprites_list = pygame.sprite.Group()
+player = Player(30, 30)
+player.rect.x = WINDOWWIDTH / 2 - player.rect.centerx
+player.rect.y = 330
+active_sprites_list.add(player)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a or event.key == pygame.K_j:
+                player.move_left(5)
+            if event.key == pygame.K_d or event.key == pygame.K_l:
+                player.move_right(5)
+            if event.key == pygame.K_w or event.key == pygame.K_i:
+                player.move_up(5)
+            if event.key == pygame.K_s or event.key == pygame.K_k:
+                player.move_down(5)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_j:
+                player.move_left(-5)
+            if event.key == pygame.K_d or event.key == pygame.K_l:
+                player.move_right(-5)
+            if event.key == pygame.K_w or event.key == pygame.K_i:
+                player.move_up(-5)
+            if event.key == pygame.K_s or event.key == pygame.K_k:
+                player.move_down(-5)
+
+
+    active_sprites_list.update()
+    DISPLAYSURF.fill(BLACK)
+    active_sprites_list.draw(DISPLAYSURF)
+    pygame.display.update()
+    FPSCLOCK.tick(FPS)
---
+++ inspiration/macgyver.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# macgyver.py
+# Author: Germain GAILLARD <gaillard.germain@gmail.com>
+# Version: 0.1
+# License: MIT
+
+"""Import modules"""
+import sys
+import pygame
+from gui import Gui
+from board import Board
+from actor import Actor
+
+
+class Macgyver:
+    """the game in pygame"""
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("MacGyver")
+
+    def main(self):
+        """main function"""
+        board = Board(15, 15, 32)
+        board.read_map('resources/structure.txt')
+        board.set_components()
+        mac = Actor(board.start)
+        gui = Gui(board.width, board.height)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if mac.status == 'alive':
+                        if event.key == pygame.K_UP:
+                            mac.move(0, -32, board)
+                        elif event.key == pygame.K_LEFT:
+                            mac.move(-32, 0, board)
+                        elif event.key == pygame.K_DOWN:
+                            mac.move(0, +32, board)
+                        elif event.key == pygame.K_RIGHT:
+                            mac.move(+32, 0, board)
+                    else:
+                        if event.key == pygame.K_RETURN:
+                            return self.main()
+                        elif event.key == pygame.K_ESCAPE:
+                            pygame.quit()
+                            sys.exit()
+
+            gui.draw(mac, board)
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    Macgyver().main()
